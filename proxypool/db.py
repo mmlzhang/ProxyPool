ba5f8a252491ffb286e396ab223f3ddbe8e04790
zhangminglu <1367000465@qq.com>
2018-10-24 16:58:34 +0800

    调整爬去机制

--- proxypool/db.py
+++ proxypool/db.py
@@ -23,7 +23,7 @@ class RedisClient(object):
         :param score: 分数
         :return: 添加结果
         """
-        if not re.match('\d+\.\d+\.\d+\.\d+\:\d+', proxy):
+        if not re.match(r'\d+\.\d+\.\d+\.\d+\:\d+', proxy):
             print('代理不符合规范', proxy, '丢弃')
             return
         if not self.db.zscore(REDIS_KEY, proxy):
